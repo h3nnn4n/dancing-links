b9a892f5955369aa6153cd78e1d32e7437b8ff9b
Renan Samuel da Silva <uber.renan@gmail.com>
2015-09-15 08:50:45 -0300

    Fixed copyright date

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /************************************************************************************
  *                                                                                  *
- *     Copyright (C) 2014-2015  Renan S. Silva                                      *
+ *     Copyright (C) 2015  Renan S. Silva                                           *
  *                                                                                  *
  *     This program is free software: you can redistribute it and/or modify         *
  *     it under the terms of the GNU General Public License as published by         *
