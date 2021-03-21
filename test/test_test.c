c1655b104ff18b0bae9687fe6505dff5d9fa3dd9
h3nnn4n <uber.renan@gmail.com>
2021-03-21 17:31:20 -0300

    Add unit tests and CI boiler plate

---
+++ test/test_test.c
@@ -0,0 +1,16 @@
+#include <unity.h>
+
+void test_test() {
+  TEST_PASS();
+}
+
+void setUp() { }
+void tearDown() {}
+
+int main() {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_test);
+
+    return UNITY_END();
+}
