92a818b03267efdecb114a48b866086832265a1e
h3nnn4n <uber.renan@gmail.com>
2021-03-22 09:33:14 -0300

    (cppcheck) Code cleanup

--- src/file_utils.c
+++ src/file_utils.c
@@ -25,56 +25,10 @@
 
 #include "file_utils.h"
 
-void ensure_directory_exists(char *directory) {
-    if (file_exists(directory)) {
-        return;
-    }
-
-    mkdir_p(directory);
-}
-
 int file_exists(char *filepath) {
     struct stat buffer = {0};
 
     return (stat(filepath, &buffer) == 0);
-}
-
-// https://gist.github.com/JonathonReinhart/8c0d90191c38af2dcadb102c4e202950
-int mkdir_p(const char *path) {
-    assert(path != NULL);
-
-    const size_t len = strlen(path);
-    char         _path[PATH_MAX];
-    char *       p;
-
-    errno = 0;
-
-    if (len > sizeof(_path) - 1) {
-        errno = ENAMETOOLONG;
-        return -1;
-    }
-
-    snprintf(_path, sizeof(_path), "%s", path);
-
-    for (p = _path + 1; *p; p++) {
-        if (*p == '/') {
-            *p = '\0';
-
-            if (mkdir(_path, S_IRWXU) != 0) {
-                if (errno != EEXIST)
-                    return -1;
-            }
-
-            *p = '/';
-        }
-    }
-
-    if (mkdir(_path, S_IRWXU) != 0) {
-        if (errno != EEXIST)
-            return -1;
-    }
-
-    return 0;
 }
 
 // https://stackoverflow.com/a/44894946
@@ -90,7 +44,6 @@ int readall(FILE *in, char **dataptr, size_t *sizeptr) {
     char * data = NULL, *temp;
     size_t size = 0;
     size_t used = 0;
-    size_t n;
 
     /* None of the parameters can be NULL. */
     if (in == NULL || dataptr == NULL || sizeptr == NULL)
@@ -119,7 +72,7 @@ int readall(FILE *in, char **dataptr, size_t *sizeptr) {
             data = temp;
         }
 
-        n = fread(data + used, 1, READALL_CHUNK, in);
+        size_t n = fread(data + used, 1, READALL_CHUNK, in);
         if (n == 0)
             break;
 
