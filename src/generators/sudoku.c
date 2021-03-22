d8ec430945110b45b6912d38beb6f7a9bd5a573c
h3nnn4n <uber.renan@gmail.com>
2021-03-21 23:20:54 -0300

    Generate cover sets for clue cells and empty cells

--- src/generators/sudoku.c
+++ src/generators/sudoku.c
@@ -26,88 +26,109 @@ void sudoku_generator() {
     clues[3][1] = 3;
     clues[3][2] = 4;
 
-    /*clues[0][2] = 2;*/
-    /*clues[1][2] = 2;*/
-    /*clues[2][2] = 2;*/
-    /*clues[3][2] = 2;*/
-
     for (int i = 0; i < 4; i++) {
-        uint16_t row      = clues[i][0];
-        uint16_t column   = clues[i][1];
-        uint16_t value    = clues[i][2];
-        uint16_t position = column * (grid_size * n_grids) + row;
+        uint16_t row    = clues[i][0];
+        uint16_t column = clues[i][1];
+        uint16_t value  = clues[i][2];
 
-        printf("%d - %d %d %d\n", i, row, column, value);
+        build_cover_set(f, row, column, value, grid_size, n_grids);
+    }
 
-        // Position Constrain
-        // Each cell has to be occupied by exactly one number
+    for (int value_i = 0; value_i < grid_size * grid_size; value_i++) {
         for (int row_i = 0; row_i < grid_size * n_grids; row_i++) {
-            for (int col_i = 0; col_i < grid_size * n_grids; col_i++) {
-                if (position == col_i * (grid_size * n_grids) + row_i) {
-                    fprintf(f, "1 ");
-                } else {
-                    fprintf(f, "0 ");
+            for (int column_i = 0; column_i < grid_size * n_grids; column_i++) {
+                int skip = 0;
+
+                for (int i = 0; i < 4; i++) {
+                    uint16_t row    = clues[i][0];
+                    uint16_t column = clues[i][1];
+                    uint16_t value  = clues[i][2];
+
+                    if (row == row_i && column == column_i && value == value_i) {
+                        skip = 1;
+                        break;
+                    }
                 }
+
+                if (skip)
+                    continue;
+
+                build_cover_set(f, row_i, column_i, value_i, grid_size, n_grids);
             }
         }
-
-        fprintf(f, "  ");
-
-        // Row constraint
-        // Each row has to have exactly one of each number
-
-        // Build an index where the row and the value are the coordinates
-        // Note that row and column are 0 indexed, value is not
-        uint16_t row_coordinate = row * (grid_size * n_grids) + value - 1;
-        for (int row_i = 0; row_i < grid_size * n_grids; row_i++) {
-            for (int value_i = 0; value_i < grid_size * n_grids; value_i++) {
-                if (row_coordinate == row_i * (grid_size * n_grids) + value_i) {
-                    fprintf(f, "1 ");
-                } else {
-                    fprintf(f, "0 ");
-                }
-            }
-        }
-
-        fprintf(f, "  ");
-
-        // Column Constraint
-        // Each column has to have exactly one of each number
-
-        // Build an index where the column and the value are the coordinates
-        // Note that row and column are 0 indexed, value is not
-        uint16_t col_coordinate = column * (grid_size * n_grids) + value - 1;
-        for (int col_i = 0; col_i < grid_size * n_grids; col_i++) {
-            for (int value_i = 0; value_i < grid_size * n_grids; value_i++) {
-                if (col_coordinate == col_i * (grid_size * n_grids) + value_i) {
-                    fprintf(f, "1 ");
-                } else {
-                    fprintf(f, "0 ");
-                }
-            }
-        }
-
-        fprintf(f, "  ");
-
-        // Block Constraint
-        // Each block has to have exactly one of each number
-        uint16_t block_coordinate = global_to_in_block_position(row, column, value, grid_size, n_grids);
-        for (int block_i = 0; block_i < grid_size * n_grids; block_i++) {
-            for (int value_i = 0; value_i < grid_size * n_grids; value_i++) {
-                if (block_coordinate == block_i * (grid_size * n_grids) + value_i) {
-                    fprintf(f, "1 ");
-                } else {
-                    fprintf(f, "0 ");
-                }
-            }
-        }
-
-        fprintf(f, "\n");
     }
 
     printf("\n");
 
     fclose(f);
+}
+
+void build_cover_set(FILE *f, uint16_t row, uint16_t column, uint16_t value, uint16_t grid_size, uint16_t n_grids) {
+    // Position Constrain
+    // Each cell has to be occupied by exactly one number
+    uint16_t position = column * (grid_size * n_grids) + row;
+    for (int row_i = 0; row_i < grid_size * n_grids; row_i++) {
+        for (int col_i = 0; col_i < grid_size * n_grids; col_i++) {
+            if (position == col_i * (grid_size * n_grids) + row_i) {
+                fprintf(f, "1 ");
+            } else {
+                fprintf(f, "0 ");
+            }
+        }
+    }
+
+    fprintf(f, "  ");
+
+    // Row constraint
+    // Each row has to have exactly one of each number
+
+    // Build an index where the row and the value are the coordinates
+    // Note that row and column are 0 indexed, value is not
+    uint16_t row_coordinate = row * (grid_size * n_grids) + value - 1;
+    for (int row_i = 0; row_i < grid_size * n_grids; row_i++) {
+        for (int value_i = 0; value_i < grid_size * n_grids; value_i++) {
+            if (row_coordinate == row_i * (grid_size * n_grids) + value_i) {
+                fprintf(f, "1 ");
+            } else {
+                fprintf(f, "0 ");
+            }
+        }
+    }
+
+    fprintf(f, "  ");
+
+    // Column Constraint
+    // Each column has to have exactly one of each number
+
+    // Build an index where the column and the value are the coordinates
+    // Note that row and column are 0 indexed, value is not
+    uint16_t col_coordinate = column * (grid_size * n_grids) + value - 1;
+    for (int col_i = 0; col_i < grid_size * n_grids; col_i++) {
+        for (int value_i = 0; value_i < grid_size * n_grids; value_i++) {
+            if (col_coordinate == col_i * (grid_size * n_grids) + value_i) {
+                fprintf(f, "1 ");
+            } else {
+                fprintf(f, "0 ");
+            }
+        }
+    }
+
+    fprintf(f, "  ");
+
+    // Block Constraint
+    // Each block has to have exactly one of each number
+    uint16_t block_coordinate = global_to_in_block_position(row, column, value, grid_size, n_grids);
+    for (int block_i = 0; block_i < grid_size * n_grids; block_i++) {
+        for (int value_i = 0; value_i < grid_size * n_grids; value_i++) {
+            if (block_coordinate == block_i * (grid_size * n_grids) + value_i) {
+                fprintf(f, "1 ");
+            } else {
+                fprintf(f, "0 ");
+            }
+        }
+    }
+
+    fprintf(f, "\n");
 }
 
 uint16_t global_to_in_block_position(uint16_t row, uint16_t column, uint16_t value, uint16_t grid_size,
